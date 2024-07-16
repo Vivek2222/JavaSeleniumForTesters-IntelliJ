a09fd505aa669bf26b713d1a64400e60b3ccb8bd
Vivek <vivekpatil2291997@gmail.com>
2024-07-16 12:44:52 +0530

    updated on 16-07-2024

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,27 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        checkdatatype();
+    }
+
+    public static void checkdatatype() {
+        int num1 = 45;
+        int num2= 50;
+        int num3 = num1 + num2;
+
+        float val1 = 23.4f;
+        float val2 = 56.7f;
+        float val3 = val1 + val2;
+
+
+        char charval1 = 'c';
+
+        boolean isvalid = true;
+
+        String s = "Vivek";
+
+        System.out.println("char is "+charval1);
+        System.out.println("Value is "+isvalid);
+        System.out.println("String is "+s);
     }
 }
