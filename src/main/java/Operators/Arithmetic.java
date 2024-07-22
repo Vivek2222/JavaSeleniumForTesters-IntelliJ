21bfe07bdbf684d3ec283b30aebffd3d6f282e05
Vivek <vivekpatil2291997@gmail.com>
2024-07-22 22:57:05 +0530

    updated on 22-07-2024

---
+++ src/main/java/Operators/Arithmetic.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z;
+
+        z = y-x;
+        int z1 = y+x;
+        int z2 = y*x;
+        int z3 = y/x;
+        int z4 = z3*x;
+
+        System.out.println(z);
+        System.out.println(z1);
+        System.out.println(z2);
+        System.out.println(z3);
+        System.out.println(z4);
+
+    }
+}
