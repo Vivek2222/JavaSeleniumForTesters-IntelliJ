a902202e1660921ab1daf7a45c19daae715c901c
Vivek <vivekpatil2291997@gmail.com>
2024-07-24 23:08:13 +0530

    updated on 24-07-2024

---
+++ src/main/java/ipod/ThisDemo.java
@@ -0,0 +1,15 @@
+package ipod;
+
+public class ThisDemo {
+
+    int x;
+    int y;
+
+    ThisDemo(int x, int y){
+        this.x=x;
+        this.y=y;
+
+        System.out.println("Value of This.x is: "+this.x);
+        System.out.println("Value of This.x is: "+this.y);
+    }
+}
