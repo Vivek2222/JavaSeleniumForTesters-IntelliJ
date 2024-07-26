bd8f09bbe5aa9527ca75b555126008acde658f0b
Vivek <vivekpatil2291997@gmail.com>
2024-07-26 21:42:05 +0530

    updated on 26-07-2024

---
+++ src/main/java/AccessModifires/ClassModifires/pckg1/Class2.java
@@ -0,0 +1,12 @@
+package AccessModifires.ClassModifires.pckg1;
+
+import AccessModifires.ClassModifires.pckg2.Class3;
+
+class Class2 {
+
+    public void method1(){
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
+        Class3 c3 = new Class3();
+    }
+}
---
+++ src/main/java/AccessModifires/ClassModifires/pckg2/Class3.java
@@ -0,0 +1,13 @@
+package AccessModifires.ClassModifires.pckg2;
+
+import AccessModifires.ClassModifires.pckg1.Class1;
+//import AccessModifires.pckg1.Class2;
+
+public class Class3 {
+
+    public void method1(){
+        Class1 c1 = new Class1();
+       // Class2 c2 = new Class2();//class2 is not a public class
+        Class3 c3 = new Class3();
+    }
+}
