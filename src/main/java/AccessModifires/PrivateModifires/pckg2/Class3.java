b2723f4b68ca352291e38bf8c07e035f35e28fd0
Vivek <vivekpatil2291997@gmail.com>
2024-07-27 15:16:48 +0530

    updated on 27-07-2024

---
+++ src/main/java/AccessModifires/PrivateModifires/pckg2/Class3.java
@@ -0,0 +1,13 @@
+package AccessModifires.PrivateModifires.pckg2;
+
+import AccessModifires.PrivateModifires.pckg1.Class1;
+
+public class Class3 {
+
+    Class1 c1 = new Class1();
+
+    public void method2(){
+//        System.out.println(c1.x);
+//        c1.method1();
+    }
+}
