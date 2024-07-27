ca3c3098f40f97c2f1cc103b4683b3a9e7b4b921
Vivek <vivekpatil2291997@gmail.com>
2024-07-27 15:16:33 +0530

    updated on 27-07-2024

---
+++ src/main/java/AccessModifires/DefaultModifier/pckg1/Class2.java
@@ -0,0 +1,11 @@
+package AccessModifires.DefaultModifier.pckg1;
+
+public class Class2 {
+
+    Class1 c1 = new Class1();
+
+    public void method2(){
+        System.out.println(c1.x);
+        c1.method1();
+    }
+}
