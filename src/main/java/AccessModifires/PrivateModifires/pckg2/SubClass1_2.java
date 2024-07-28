3f316fd96fb06004b9962fe64b886f0e0125d327
Vivek <vivekpatil2291997@gmail.com>
2024-07-28 10:42:26 +0530

    updated on 28-07-2024

---
+++ src/main/java/AccessModifires/PrivateModifires/pckg2/SubClass1_2.java
@@ -0,0 +1,11 @@
+package AccessModifires.PrivateModifires.pckg2;
+
+import AccessModifires.PrivateModifires.pckg1.Class1;
+
+public class SubClass1_2 extends Class1 {
+
+    public void method2(){
+//        System.out.println(x);
+//        method1();
+    }
+}
