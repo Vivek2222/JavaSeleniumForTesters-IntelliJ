69b971016c8e915641aca71abd7946bf0a538076
Vivek <vivekpatil2291997@gmail.com>
2024-08-01 21:41:43 +0530

    updated on 01-08-2024

---
+++ src/main/java/AccessModifires/DefaultModifier/pckg2/SubClass1_2.java
@@ -0,0 +1,20 @@
+package AccessModifires.DefaultModifier.pckg2;
+
+import AccessModifires.DefaultModifier.pckg1.Class1;
+import AccessModifires.DefaultModifier.pckg1.Class2;
+import AccessModifires.DefaultModifier.pckg1.SubClass1_1;
+
+//Outside package variable and method are not allowed
+public class SubClass1_2 extends Class1 {
+
+//    public void method2(){
+//        System.out.println(x);
+//        method1();
+//    }
+
+//    Class1 c1 = new Class1();
+//    public void method2(){
+//        System.out.println(x);
+//        method1();
+    }
+//}
