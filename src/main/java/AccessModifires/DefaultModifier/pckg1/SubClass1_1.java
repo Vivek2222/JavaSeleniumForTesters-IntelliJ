9d17de41dbda5e217f9d534fd9c05340257ecafb
Vivek <vivekpatil2291997@gmail.com>
2024-08-01 21:41:28 +0530

    updated on 01-08-2024

---
+++ src/main/java/AccessModifires/DefaultModifier/pckg1/SubClass1_1.java
@@ -0,0 +1,9 @@
+package AccessModifires.DefaultModifier.pckg1;
+
+public class SubClass1_1 extends Class1 {
+
+    public void method2(){
+        System.out.println(x);
+        method1();
+    }
+}
