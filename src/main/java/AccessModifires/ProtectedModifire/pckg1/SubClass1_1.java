72ce8a41a1aabfc6ed44b725da6d11903386ec40
Vivek <vivekpatil2291997@gmail.com>
2024-08-02 14:18:29 +0530

    updated on 02-08-2024

---
+++ src/main/java/AccessModifires/ProtectedModifire/pckg1/SubClass1_1.java
@@ -0,0 +1,9 @@
+package AccessModifires.ProtectedModifire.pckg1;
+
+public class SubClass1_1 extends Class1 {
+
+    public void method2(){
+        System.out.println(x);
+        method1();
+    }
+}
