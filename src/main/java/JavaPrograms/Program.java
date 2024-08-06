a75eb63c4ae307db0da9d2f26eff423a0eff9ac2
Vivek <vivekpatil2291997@gmail.com>
2024-08-06 09:07:49 +0530

    updated on 06-08-2024

---
+++ src/main/java/JavaPrograms/Program.java
@@ -0,0 +1,11 @@
+package JavaPrograms;
+
+public class Program {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[0]='a';
+        a[1]='b';
+//ASCII Values a[0]=97 a[1]=98 a[2]=0(default initialization)
+        System.out.println(a[0]+a[1]+a[2]);
+    }
+}
