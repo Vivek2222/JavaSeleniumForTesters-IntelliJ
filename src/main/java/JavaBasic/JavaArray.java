3b9cd166f70b82e7e8e1d2326b9a0ca1da496571
Vivek <vivekpatil2291997@gmail.com>
2024-08-07 12:00:32 +0530

    updated on 07-08-2024

---
+++ src/main/java/JavaBasic/JavaArray.java
@@ -0,0 +1,30 @@
+package JavaBasic;
+
+public class JavaArray {
+
+    public static void main(String[] args) {
+
+        int[] num;
+        num = new int[4];
+        num[0] = 10;
+
+        int[] num2 = new int[] {3, 4, 5, 6};
+        int[] num3 = new int[6];
+        int[] num4 = {10,11,12};
+
+        for(int i = 0; i<6; i++){
+           num3[i]= i+10;
+        }
+
+        for (int i: num3){
+            System.out.println(i);
+        }
+        System.out.println("==========================================================");
+        for (int i: num4){
+            System.out.println(i);
+        }
+        //Array is not growable
+        String[] names = new String[] {"Vivek", "Mania", "Raviv"};
+        System.out.println(names.length);
+    }
+}
