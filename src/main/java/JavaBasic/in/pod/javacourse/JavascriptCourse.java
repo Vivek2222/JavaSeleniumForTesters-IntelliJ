ac6c831e3d8752f62148859df5eadc1f9404595a
Vivek <vivekpatil2291997@gmail.com>
2024-08-09 22:54:03 +0530

    updated on 09-08-2024

---
+++ src/main/java/JavaBasic/in/pod/javacourse/JavascriptCourse.java
@@ -0,0 +1,11 @@
+package JavaBasic.in.pod.javacourse;
+
+import JavaBasic.javacourse.CourseJava;
+
+public class JavascriptCourse {
+
+    public void demo(){
+
+        CourseJava j1 = new CourseJava();
+    }
+}
