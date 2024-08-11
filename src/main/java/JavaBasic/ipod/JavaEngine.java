0f4ec97f9ac0c617f6a7a454f9ab83c29dbe18a8
Vivek <vivekpatil2291997@gmail.com>
2024-08-11 13:04:15 +0530

    updated on 11-08-2024

---
+++ src/main/java/JavaBasic/ipod/JavaEngine.java
@@ -0,0 +1,28 @@
+package JavaBasic.ipod;
+
+
+public class JavaEngine {
+
+    public static void main(String[] args) {
+
+        //JavaCourse1 j1 = new JavaCourse1();
+        //j1.perc = 10;
+        //System.out.println(j1.perc);
+//        JavaCourse1.perc = 100;
+        //System.out.println(JavaCourse1.perc);
+
+//        JavaCourse1 j1 = new JavaCourse1();
+//        JavaCourse1 j2 = new JavaCourse1();
+//
+//        j1.num = 5;
+//        System.out.println(j1.num);
+//        System.out.println(j1.perc);
+//        System.out.println(j2.num);
+//
+//        JavaCourse1.getNum();
+
+        ThisDemo d1 = new ThisDemo(5,6);
+        ThisDemo d2 = new ThisDemo(10, 12);
+
+    }
+}
