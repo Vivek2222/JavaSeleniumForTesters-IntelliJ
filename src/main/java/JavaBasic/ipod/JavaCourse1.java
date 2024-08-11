0d2b695d12acb8faa4e0833aa9e469c1910bd9a5
Vivek <vivekpatil2291997@gmail.com>
2024-08-11 13:04:00 +0530

    updated on 11-08-2024

---
+++ src/main/java/JavaBasic/ipod/JavaCourse1.java
@@ -0,0 +1,16 @@
+package JavaBasic.ipod;
+
+public class JavaCourse1 {
+
+    public int num;
+
+    public static int perc;
+
+    public void getInfo(){
+        System.out.println("Hello World"+ num);
+    }
+
+    public static void getNum(){
+        System.out.println("Name is Vivek");
+    }
+}
