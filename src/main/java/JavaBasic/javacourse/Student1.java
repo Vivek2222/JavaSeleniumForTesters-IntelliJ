0c27733b0f5ce6a823214e241d4db03fdb37a061
Vivek <vivekpatil2291997@gmail.com>
2024-08-13 23:12:00 +0530

    updated on 13-08-2024

---
+++ src/main/java/JavaBasic/javacourse/Student1.java
@@ -0,0 +1,19 @@
+package JavaBasic.javacourse;
+
+public class Student1 {
+
+    public static void main(String[] args) {
+        System.out.println("Arguments passed are:");
+        for (String s: args){
+            System.out.println(s);
+        }
+    }
+
+
+//    public static void method(String[] args){
+//        System.out.println("Arguments passed are:");
+//        for (String s: args) {
+//            System.out.println(s);
+//        }
+//    }
+}
