2d7227d287229dee62ce13ec044effbea3c7e7b6
Vivek <vivekpatil2291997@gmail.com>
2024-08-14 10:41:21 +0530

    updated on 14-08-2024

---
+++ src/main/java/Variables/InstanceVariables.java
@@ -0,0 +1,36 @@
+package Variables;
+
+public class InstanceVariables {
+
+    public int x;
+
+    public float y;
+
+    String name;
+
+    char char1;
+
+    boolean isValid;
+
+    public void method1(){
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+
+        InstanceVariables c1 = new InstanceVariables();
+        InstanceVariables c2 = new InstanceVariables();
+        InstanceVariables c3 = new InstanceVariables();
+
+        System.out.println(c1.x);
+        System.out.println(c1.y);
+        System.out.println(c1.name);
+        System.out.println(c1.char1);
+        System.out.println(c1.isValid);
+
+        c1.x = 10;
+
+        System.out.println(c1.x);
+        System.out.println(c2.x);
+    }
+}
