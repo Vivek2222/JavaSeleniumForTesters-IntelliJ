506f28de5ebe1d93f4a0b0854d32d75946c219d8
Vivek <vivekpatil2291997@gmail.com>
2024-08-16 23:49:26 +0530

    updated on 16-08-2024

---
+++ src/main/java/Variables/StaticVariables.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class StaticVariables {
+
+    static int x;
+
+    public static void main(String[] args) {
+
+        StaticVariables s1 = new StaticVariables();
+        StaticVariables s2 = new StaticVariables();
+
+        //s1.x=10; //not required to create the objects
+        //System.out.println(s1.x);
+        //System.out.println(s2.x);
+        StaticVariables.x=10;
+
+        System.out.println(x);
+        System.out.println(x);
+    }
+}
