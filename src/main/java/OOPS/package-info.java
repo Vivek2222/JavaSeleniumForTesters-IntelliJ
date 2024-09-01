0f0582fcf969414293606a153aa77cc658f7e4ab
Vivek <vivekpatil2291997@gmail.com>
2024-09-01 16:35:33 +0530

    updated on 01-09-2024

---
+++ src/main/java/OOPS/package-info.java
@@ -0,0 +1,10 @@
+package OOPS;
+//Main goal of OPPs is Reusability and extensibilty
+//Encapsulation: Data Hiding outsider cannot access it. manage the control
+//Abstraction: Don't expose the logic of the method.
+//Inheritance: Parent and child relation. child class gets the methods and variables from the parents.
+//Polymorphism: Many form. single entity can achieve multiple forms.
+
+//Form of Polymorphism
+//Overloading
+//Overriding
