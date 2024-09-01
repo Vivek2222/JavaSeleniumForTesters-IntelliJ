9eecae9c9a12d42f5d7a5be86b15a85b9c25febb
Vivek <vivekpatil2291997@gmail.com>
2024-09-01 16:35:16 +0530

    updated on 01-09-2024

---
+++ src/main/java/Collection/ObjectDemo.java
@@ -0,0 +1,20 @@
+package Collection;
+
+public class ObjectDemo {
+
+    public static void main(String[] args) {
+
+        Object obj = new Object();
+
+        String s1 = "Vivek";
+
+        ObjectDemo od1 = new ObjectDemo();
+
+        od1.hashCode();
+        od1.toString();
+        od1.equals(s1);
+
+
+        //Every class in java inherited the method from Object class.
+    }
+}
