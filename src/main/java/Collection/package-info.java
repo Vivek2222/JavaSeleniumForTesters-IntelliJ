37e43163443be4d31494a9c912e4cb4afb963451
Vivek <vivekpatil2291997@gmail.com>
2024-09-07 12:12:43 +0530

    Updated on 07-09-2024

---
+++ src/main/java/Collection/package-info.java
@@ -0,0 +1,30 @@
+package Collection;
+
+
+
+//Collection: Group of objects
+//Array also help in grouping the objects
+//Array is not growable and depend on indexing
+
+//Java Collection Framework
+/*
+
+Collection - Interface
+Set <I> - Need to have a unique value
+->HashSet(Class)
+
+List<I> - It can have duplicate value and any value
+->ArrayList(Class)
+
+Queue<I> - Key-Value pair
+LinkedList(Class)
+
+//Generic - To make collection datatype Safe
+Specified the Datatype for collection
+Set<String> s = new HashSet<String>();
+Set<String> s = new HashSet<>();
+
+
+
+
+ */
