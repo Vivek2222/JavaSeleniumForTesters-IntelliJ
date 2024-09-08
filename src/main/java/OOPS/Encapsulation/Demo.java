01e568510fe07363628a79e6dd48c71ac232e2c9
Vivek <vivekpatil2291997@gmail.com>
2024-09-08 10:59:22 +0530

    Updated on 08-09-2024

---
+++ src/main/java/OOPS/Encapsulation/Demo.java
@@ -0,0 +1,13 @@
+package OOPS.Encapsulation;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Encapsulation d1 = new Encapsulation();
+
+        d1.setX(6);
+
+        System.out.println(d1.getX());
+    }
+}
