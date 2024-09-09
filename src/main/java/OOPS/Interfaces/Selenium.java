f459a5ed7284c0b0eeaf5e143144511c9cc30853
Vivek <vivekpatil2291997@gmail.com>
2024-09-09 11:09:33 +0530

    Updated on 09-09-2024

---
+++ src/main/java/OOPS/Interfaces/Selenium.java
@@ -0,0 +1,29 @@
+package OOPS.Interfaces;
+
+public class Selenium {
+
+
+    public static void main(String[] args) {
+
+//        ChromeBrowser chrome = new ChromeBrowser();
+//        FirefoxBrowser firefox = new FirefoxBrowser();
+
+//        chrome.console();
+//        chrome.userInterface();
+//
+//        firefox.console();
+//        firefox.userInterface();
+
+        String browserName = "chrome";
+        Browser b1 = null;
+        if (browserName == "chrome"){
+            b1 = new ChromeBrowser();
+        }
+        else{
+            b1 = new FirefoxBrowser();
+        }
+
+        b1.console();
+        b1.userInterface();
+    }
+}
