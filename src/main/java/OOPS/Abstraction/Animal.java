12a2bcaf4d781a639a2ed7975ca72b743efe124a
Vivek <vivekpatil2291997@gmail.com>
2024-09-10 09:46:40 +0530

    Updated on 10-09-2024

---
+++ src/main/java/OOPS/Abstraction/Animal.java
@@ -0,0 +1,11 @@
+package OOPS.Abstraction;
+
+public abstract class Animal {
+
+    //Abstarct class use to hide details
+    public void eat(){
+        System.out.println("Eating");
+    }
+
+    public abstract void walk();
+}
