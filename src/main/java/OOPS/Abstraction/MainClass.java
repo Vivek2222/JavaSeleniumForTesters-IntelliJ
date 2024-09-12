63611d99d6dc33835c6e391cb5de871222e70647
Vivek <vivekpatil2291997@gmail.com>
2024-09-12 15:08:29 +0530

    Updated on 12-09-2024

---
+++ src/main/java/OOPS/Abstraction/MainClass.java
@@ -0,0 +1,14 @@
+package OOPS.Abstraction;
+
+import OOPS.Abstraction.Animal;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        //Animal a1 = new Animal();
+        Animal a2 = new Dog();
+        a2.eat();
+        a2.walk();
+    }
+}
