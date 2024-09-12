54bec55f3334c5d66b2e3113691479b8e892dce6
Vivek <vivekpatil2291997@gmail.com>
2024-09-13 00:42:35 +0530

    Updated on 13-09-2024

---
+++ src/main/java/OOPS/Encapsulation/Encapsulation.java
@@ -0,0 +1,18 @@
+package OOPS.Encapsulation;
+
+public class Encapsulation {
+
+    private int x;
+
+    private String name;
+
+    public void setX(int x){
+
+        this.x=x;
+    }
+
+    public int getX(){
+        return this.x;
+        //return x;
+    }
+}
