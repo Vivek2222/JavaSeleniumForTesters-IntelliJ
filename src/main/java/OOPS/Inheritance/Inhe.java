d83ba7a27ecdf03860d63c9ec07fa2977aa6d987
Vivek <vivekpatil2291997@gmail.com>
2024-09-20 00:09:50 +0530

    Updated on 20-09-2024

---
+++ src/main/java/OOPS/Inheritance/Inhe.java
@@ -0,0 +1,15 @@
+package OOPS.Inheritance;
+
+public class Inhe {
+
+
+    public static void main(String[] args) {
+        BMW car = new BMW();
+        car.drive();
+        car.playMusic();
+
+        Ferrari car2 = new Ferrari();
+        car2.drive();
+        car2.playMusic();
+    }
+}
