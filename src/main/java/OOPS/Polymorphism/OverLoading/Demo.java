bca7c8d71af0971f4f91b7c7f7f2d6ec7f4896ec
Vivek <vivekpatil2291997@gmail.com>
2024-09-21 10:27:46 +0530

    Updated on 21-09-2024

---
+++ src/main/java/OOPS/Polymorphism/OverLoading/Demo.java
@@ -0,0 +1,13 @@
+package OOPS.Polymorphism.OverLoading;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        OverLoading demo = new OverLoading();
+        demo.walk();
+        demo.walk(5);
+        demo.walk("Vivek");
+        demo.walk(true);
+    }
+}
