b6ef2b085dae4c47cf429495b62f2f0d41f8bd3f
Vivek <vivekpatil2291997@gmail.com>
2024-09-22 12:16:05 +0530

    Updated on 22-09-2024

---
+++ src/main/java/OOPS/Polymorphism/AbstractPoly/Dog.java
@@ -0,0 +1,14 @@
+package OOPS.Polymorphism.AbstractPoly;
+
+public class Dog extends Animal {
+
+    public void walk(){
+
+        System.out.println("Dog is walking");
+    }
+
+    public void bark(){
+
+        System.out.println("Bark");
+    }
+}
