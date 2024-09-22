0b5a0958ecf86d1deb8f8e2e571a8efdf6cc78a7
Vivek <vivekpatil2291997@gmail.com>
2024-09-22 12:15:35 +0530

    Updated on 22-09-2024

---
+++ src/main/java/OOPS/Polymorphism/AbstractPoly/Cat.java
@@ -0,0 +1,14 @@
+package OOPS.Polymorphism.AbstractPoly;
+
+public class Cat extends Animal{
+
+    public void meow(){
+
+        System.out.println("Meoww");
+    }
+
+    public void walk(){
+
+        System.out.println("Cat is walking");
+    }
+}
