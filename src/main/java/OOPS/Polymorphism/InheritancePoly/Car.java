77d74c26acc778042dcd3955a2ea48905d49f282
Vivek <vivekpatil2291997@gmail.com>
2024-09-25 00:02:07 +0530

    Updated on 25-09-2024

---
+++ src/main/java/OOPS/Polymorphism/InheritancePoly/Car.java
@@ -0,0 +1,14 @@
+package OOPS.Polymorphism.InheritancePoly;
+
+public class Car {
+
+    public int carNumber;
+
+    public void drive() {
+        System.out.println("Car is driving");
+    }
+
+    public void playMusic() {
+        System.out.println("Audio Music is playing ");
+    }
+}
