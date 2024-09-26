3b798152911e1fa92370f1224d59ee3e08d02e50
Vivek <vivekpatil2291997@gmail.com>
2024-09-26 12:07:02 +0530

    Updated on 26-09-2024

---
+++ src/main/java/OOPS/Polymorphism/InheritancePoly/Ferrari.java
@@ -0,0 +1,11 @@
+package OOPS.Polymorphism.InheritancePoly;
+
+public class Ferrari extends Car {
+
+    public void drive() {
+
+        super.drive();
+        System.out.println("Ferrari is driving");
+    }
+
+}
