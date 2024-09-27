d43a007aba342bc6674982a4d040960d5c94e563
Vivek <vivekpatil2291997@gmail.com>
2024-09-27 18:40:14 +0530

    Updated on 27-09-2024

---
+++ src/main/java/OOPS/Polymorphism/InheritancePoly/Inhe.java
@@ -0,0 +1,14 @@
+package OOPS.Polymorphism.InheritancePoly;
+
+public class Inhe {
+
+
+    public static void main(String[] args) {
+        Car car = new BMW();
+        car.drive();
+
+        Car car1= new Ferrari();
+        car1.drive();
+
+    }
+}
