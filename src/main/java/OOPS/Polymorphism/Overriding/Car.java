9f240c209658f0f2a4ccb563012f77f2a4bb7be5
Vivek <vivekpatil2291997@gmail.com>
2024-09-28 00:02:59 +0530

    Updated on 28-09-2024

---
+++ src/main/java/OOPS/Polymorphism/Overriding/Car.java
@@ -0,0 +1,14 @@
+package OOPS.Polymorphism.Overriding;
+
+public class Car {
+
+    public int carNumber;
+
+    public void drive() {
+        System.out.println("Car is driving");
+    }
+
+    public void playMusic() {
+        System.out.println("Audio Music is playing");
+    }
+}
