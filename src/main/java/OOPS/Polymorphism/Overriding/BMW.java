4199611d7a016a06abdcccf606850366499915aa
Vivek <vivekpatil2291997@gmail.com>
2024-09-28 00:02:27 +0530

    Updated on 28-09-2024

---
+++ src/main/java/OOPS/Polymorphism/Overriding/BMW.java
@@ -0,0 +1,18 @@
+package OOPS.Polymorphism.Overriding;
+
+public class BMW extends Car {
+
+    String dashboardType;
+
+    public void playVideoMusic() {
+
+        System.out.println("Music video is playing");
+    }
+
+    public void drive(int x) {
+
+        System.out.println("BMW is driving");
+    }
+
+
+}
