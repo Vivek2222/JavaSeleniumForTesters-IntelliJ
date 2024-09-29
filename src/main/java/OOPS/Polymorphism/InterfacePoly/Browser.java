ee49374d47bf760ccbcd9f15abf85bf066077377
Vivek <vivekpatil2291997@gmail.com>
2024-09-29 19:53:31 +0530

    Updated on 29-09-2024

---
+++ src/main/java/OOPS/Polymorphism/InterfacePoly/Browser.java
@@ -0,0 +1,16 @@
+package OOPS.Polymorphism.InterfacePoly;
+
+//100% abstraction achieved
+//Instantiate not possible
+//Cannot use extends
+//can user implement
+//single class can have multiple interfaces
+//With public, private keyword consider as public only
+
+public interface Browser {
+
+    void console();
+
+    void userInterface();
+
+}
