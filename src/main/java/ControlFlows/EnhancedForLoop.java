0817f83cee8ba57167e0162ca340cc67648250c9
Vivek Patil <vivekpatil2291997@gmail.com>
2024-11-09 12:35:01 +0530

    Updated on 09-11-2024 update format

--- src/main/java/ControlFlows/EnhancedForLoop.java
+++ src/main/java/ControlFlows/EnhancedForLoop.java
@@ -5,11 +5,11 @@ public class EnhancedForLoop {
     public static void main(String[] args) {
 
         int[] numArray = new int[6];
-        for(int i = 0; i<numArray.length; i++){
+        for (int i = 0; i < numArray.length; i++) {
             numArray[i] = i + 10;
         }
 
-        for(int x: numArray){//we cannot print the index position like for loop
+        for (int x : numArray) {//we cannot print the index position like for loop
             System.out.println(x);
         }
     }
