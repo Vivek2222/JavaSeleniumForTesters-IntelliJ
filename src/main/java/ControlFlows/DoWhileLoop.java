b4e1909c106b0026fb5fc680e53329b0e4155fd1
Vivek Patil <vivekpatil2291997@gmail.com>
2024-11-09 12:34:36 +0530

    Updated on 09-11-2024 update format

--- src/main/java/ControlFlows/DoWhileLoop.java
+++ src/main/java/ControlFlows/DoWhileLoop.java
@@ -6,12 +6,12 @@ public class DoWhileLoop {
 
         int x = 0;
 
-        while (x>0){//first check the cond
+        while (x > 0) {//first check the cond
             System.out.println("While: Executed");
         }
 
         do {//first executed and then check the condition
             System.out.println("Do While: Executed");
-        }while (x>0);
+        } while (x > 0);
     }
 }
