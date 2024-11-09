979a819b640d70fa404295cc37bea8af398d47bb
Vivek Patil <vivekpatil2291997@gmail.com>
2024-11-09 12:32:04 +0530

    Updated on 09-11-2024 update format

--- src/main/java/ExceptionalHandling/ThrowsKeyword.java
+++ src/main/java/ExceptionalHandling/ThrowsKeyword.java
@@ -8,24 +8,24 @@ public class ThrowsKeyword {
         System.out.println("In Main Method");
         try {
             c1.method2(0);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("In Catch");
         }
         System.out.println("In Main Method 2");
     }
 
-    public void method2(int y) throws ArithmeticException, ArrayIndexOutOfBoundsException{
+    public void method2(int y) throws ArithmeticException, ArrayIndexOutOfBoundsException {
         method1(y);
     }
 
-    public void method1(int x) throws ArithmeticException, ArrayIndexOutOfBoundsException{
-            int y = 0;
-            int[] numArray = new int[6];
-            System.out.println(numArray[7]);
+    public void method1(int x) throws ArithmeticException, ArrayIndexOutOfBoundsException {
+        int y = 0;
+        int[] numArray = new int[6];
+        System.out.println(numArray[7]);
 
-            y = 100 / x;
-            System.out.println("Value of Y is: " + y);
+        y = 100 / x;
+        System.out.println("Value of Y is: " + y);
 
         System.out.println("I am inside method1");
     }
