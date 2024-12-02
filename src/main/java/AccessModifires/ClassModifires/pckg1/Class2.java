9306135cc2ae627bba2418bb75f548b664db5e69
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-02 11:41:06 +0530

    Updated on 02-12-2024 update format

--- src/main/java/AccessModifires/ClassModifires/pckg1/Class2.java
+++ src/main/java/AccessModifires/ClassModifires/pckg1/Class2.java
@@ -4,7 +4,7 @@ import AccessModifires.ClassModifires.pckg2.Class3;
 
 class Class2 {
 
-    public void method1(){
+    public void method1() {
         Class1 c1 = new Class1();
         Class2 c2 = new Class2();
         Class3 c3 = new Class3();
