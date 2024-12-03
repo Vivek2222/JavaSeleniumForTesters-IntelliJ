09d3970884ae1079de9b395c8fc7a36547cde16c
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-04 00:10:01 +0530

    Updated on 04-12-2024 update format

--- src/main/java/ControlFlows/WhileLoop.java
+++ src/main/java/ControlFlows/WhileLoop.java
@@ -5,14 +5,14 @@ public class WhileLoop {
     public static void main(String[] args) {
 
         int[] numArray = new int[6];
-        for(int i = 0; i<numArray.length; i++){
+        for (int i = 0; i < numArray.length; i++) {
             numArray[i] = i + 10;
         }
 
-        int z =0;
-        while(z < numArray.length){
-            System.out.println("Index: "+z);
-            System.out.println("Value: "+numArray[z]);
+        int z = 0;
+        while (z < numArray.length) {
+            System.out.println("Index: " + z);
+            System.out.println("Value: " + numArray[z]);
             z++;
         }
     }
