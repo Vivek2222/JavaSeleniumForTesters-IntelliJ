1f906ee7b4ec3febb8bf902214ecb74fc88c8b4c
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-10 03:29:12 +0530

    Updated on 10-12-2024 update format

--- src/main/java/JavaBasic/JavaArray.java
+++ src/main/java/JavaBasic/JavaArray.java
@@ -8,23 +8,23 @@ public class JavaArray {
         num = new int[4];
         num[0] = 10;
 
-        int[] num2 = new int[] {3, 4, 5, 6};
+        int[] num2 = new int[]{3, 4, 5, 6};
         int[] num3 = new int[6];
-        int[] num4 = {10,11,12};
+        int[] num4 = {10, 11, 12};
 
-        for(int i = 0; i<6; i++){
-           num3[i]= i+10;
+        for (int i = 0; i < 6; i++) {
+            num3[i] = i + 10;
         }
 
-        for (int i: num3){
+        for (int i : num3) {
             System.out.println(i);
         }
         System.out.println("==========================================================");
-        for (int i: num4){
+        for (int i : num4) {
             System.out.println(i);
         }
         //Array is not growable
-        String[] names = new String[] {"Vivek", "Mania", "Raviv"};
+        String[] names = new String[]{"Vivek", "Mania", "Raviv"};
         System.out.println(names.length);
     }
 }
