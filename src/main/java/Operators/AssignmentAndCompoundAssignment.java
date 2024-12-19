168785511bfb53f48a644d000362465b65c151c6
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-20 02:03:13 +0530

    Updated on 20-12-2024 update format

--- src/main/java/Operators/AssignmentAndCompoundAssignment.java
+++ src/main/java/Operators/AssignmentAndCompoundAssignment.java
@@ -9,23 +9,21 @@ public class AssignmentAndCompoundAssignment {
         int z = 100;
 
         int m = 0;
-        m = m+y; //20
+        m = m + y; //20
         System.out.println(m);//20
-        m+=y;
+        m += y;
         System.out.println(m);//40
 
-        z-=y; //z=z-y
+        z -= y; //z=z-y
         System.out.println(z);//80
 
-        z*=y; //z=z*y =80*20; 1600
-        System.out.println("Value of z is: "+z);
+        z *= y; //z=z*y =80*20; 1600
+        System.out.println("Value of z is: " + z);
 
-        z/=y; //z=z/y; 1600/20 = 80
+        z /= y; //z=z/y; 1600/20 = 80
         System.out.println(z);
 
     }
-
-
 
 
 }
