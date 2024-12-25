0121fbb488a39c0404c87e74e38df4787ad2c756
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-26 01:03:25 +0530

    Updated on 26-12-2024 update format

--- src/main/java/OOPS/Interfaces/FirefoxBrowser.java
+++ src/main/java/OOPS/Interfaces/FirefoxBrowser.java
@@ -2,12 +2,12 @@ package OOPS.Interfaces;
 
 public class FirefoxBrowser implements Browser {
 
-    public void console(){
+    public void console() {
 
         System.out.println("I am FireFox Console");
     }
 
-    public void userInterface(){
+    public void userInterface() {
 
         System.out.println("I am FireFox userInterface");
     }
