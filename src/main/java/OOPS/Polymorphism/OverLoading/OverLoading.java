8b8df664a9c34c040ab75c05eb299714f9257b9b
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-27 02:37:52 +0530

    Updated on 27-12-2024 update format

--- src/main/java/OOPS/Polymorphism/OverLoading/OverLoading.java
+++ src/main/java/OOPS/Polymorphism/OverLoading/OverLoading.java
@@ -1,26 +1,26 @@
 package OOPS.Polymorphism.OverLoading;
+
 //Overloading means multiple methods with the same name but different parameter lists within the same class
 public class OverLoading {
 
-    public void walk(){
+    public void walk() {
 
         System.out.println("Walking");
     }
 
-    public void walk(int x){
+    public void walk(int x) {
 
         System.out.println("Variable int x is Walking");
     }
 
-    public void walk(String x){
+    public void walk(String x) {
 
         System.out.println("Variable string x is Walking");
     }
 
-    public String walk(Boolean x){
+    public String walk(Boolean x) {
         return "Walking";
     }
-
 
 
 }
