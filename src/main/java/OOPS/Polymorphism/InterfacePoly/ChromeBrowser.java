5d96f77e844c3048ae96a68d5c4380638e9715ed
Vivek Patil <vivekpatil2291997@gmail.com>
2024-12-28 13:15:49 +0530

    Updated on 28-12-2024 update format

--- src/main/java/OOPS/Polymorphism/InterfacePoly/ChromeBrowser.java
+++ src/main/java/OOPS/Polymorphism/InterfacePoly/ChromeBrowser.java
@@ -2,12 +2,12 @@ package OOPS.Polymorphism.InterfacePoly;
 
 public class ChromeBrowser implements Browser {
 
-    public void console(){
+    public void console() {
 
         System.out.println("I am Chrome Console");
     }
 
-    public void userInterface(){
+    public void userInterface() {
 
         System.out.println("I am Chrome userInterface");
     }
